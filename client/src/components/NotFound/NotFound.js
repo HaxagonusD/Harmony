b8a7f01396c8e8d4bf360eeb75656bf0926a5a67
DianaG <diana.cg93@gmail.com>
2020-12-16 02:05:17 -0500

    styling for 404 page

--- client/src/components/NotFound/NotFound.js
+++ client/src/components/NotFound/NotFound.js
@@ -5,11 +5,15 @@ import {NavLink } from "react-router-dom"
 const NotFound = () => {
   return (
     <div className="NotFound">
-      <NavLink exact to="/" activeClassName="active">
-  
-          Home
+      <nav className="nav">
+        <NavLink exact to="/" id="home">
+            Home
         </NavLink>
-      <div className="message">404! Get out of here!</div>
+      </nav>
+      <div className="message">
+        <h1 style={{color:"#55BA55"}}>404!</h1> 
+        <h1>Get out of here!</h1>
+      </div>
       <img alt="notfound" src={girlImage} />
     </div>
   );
