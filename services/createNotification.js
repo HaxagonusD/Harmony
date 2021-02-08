e37bd87d62fa0b3fa2f67bd59bb2015110d4d289
HaxagonusD <jquezad1@binghamton.edu>
2021-02-08 01:19:03 -0500

    Added comments to createNotification

--- services/createNotification.js
+++ services/createNotification.js
@@ -1,18 +1,23 @@
 const Notification = require("../database/Models/NotificationModel");
 const FindOneUserByID = require("../database/Queries/FindOneUserByID");
+//function to create notifications and add it to user
 const createNotifcation = async (ownerId, message, type) => {
+  //creating notificaiton
   const notificationDocument = new Notification({
     ownerId,
     message,
     type,
   });
+  //save the notification
   const savedNotification = await notificationDocument.save();
   if (savedNotification) {
+    //if saving the notifcation was sucessfull add the notification to the user
     const ownerDocument = await FindOneUserByID(ownerId);
     ownerDocument.notifications.push(savedNotification);
     const savedOwnerDocument = await ownerDocument.save();
     return savedOwnerDocument;
   } else {
+    //otherwise return false
     return false;
   }
 };
